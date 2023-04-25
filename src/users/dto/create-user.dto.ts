090a1c7ef6a2b467524664560e89131fbfbc9c93
danil@symfio.de <danil@symfio.de>
2023-04-25 23:14:31 +0200

    feat(users): Users finished

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ default: 'bobos@mail.bo' })
+  email: string;
+
+  @ApiProperty({ default: 'Bob Obst' })
+  fullName: string;
+
+  @ApiProperty({ default: '123456' })
+  password: string;
+}
