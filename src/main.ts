0ddd4f2e0f0cbe9cca0157cc8d7876531e9b5fd8
danil@symfio.de <danil@symfio.de>
2023-04-25 23:13:24 +0200

    feat(app): Swagger options addded etc.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
 
+  app.enableCors({ credentials: true, origin: true });
+
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Cloud Storage')
     .setVersion('1.0')
-    .addTag('cats')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
 
-  app.enableCors({ credentials: true, origin: true });
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      persistAutorization: true,
+    },
+  });
 
   await app.listen(7777);
 }
